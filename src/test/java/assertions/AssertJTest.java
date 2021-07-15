4e5df6f02405b404bc53fd7f4070a40f33abe2e0
boiarshinov <artem.borya@gmail.com>
2021-07-15 19:58:40 +0300

    Add several tests for assertion libraries AssertJ and Hamcrest

---
+++ src/test/java/assertions/AssertJTest.java
@@ -0,0 +1,34 @@
+package assertions;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class AssertJTest {
+
+	@Test
+	void objectAssert() {
+		assertThat( new Object() ).isNotEqualTo( new Object() );
+	}
+
+	@Test
+	void collectionsAssert() {
+		List<Integer> list = List.of( 1, 2, 3 );
+		Set<Integer> set = Set.of( 3, 2, 1 );
+
+		assertThat( list ).containsExactlyInAnyOrderElementsOf( set );
+	}
+
+	@Test
+	@Disabled
+	void collectionsAssertFail() {
+		List<Integer> list = List.of( 1, 2, 3, 4, 5 );
+		Set<Integer> set = Set.of( 1, 3, 5 );
+
+		assertThat( list ).containsExactlyInAnyOrderElementsOf( set );
+	}
+}
---
+++ src/test/java/assertions/HamcrestTest.java
@@ -0,0 +1,36 @@
+package assertions;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class HamcrestTest {
+
+	@Test
+	void objectAssert() {
+		assertThat( new Object(), not( new Object() ) );
+	}
+
+	@Test
+	@Disabled
+	void collectionsAssert() {
+		List<Integer> list = List.of( 1, 2, 3 );
+		Set<Integer> set = Set.of( 3, 2, 1 );
+
+		assertThat( list, containsInAnyOrder(set) );
+	}
+
+	@Test
+	@Disabled
+	void collectionsAssertNotFail() {
+		List<Integer> list = List.of( 1, 2, 3, 4, 5 );
+		Set<Integer> set = Set.of( 3, 2, 1 );
+
+		assertThat( list, containsInAnyOrder(set) );
+	}
+}
