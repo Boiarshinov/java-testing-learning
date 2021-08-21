0b4caa1def99c7ae100b2ab2979fc83d59255565
boiarshinov <artem.borya@gmail.com>
2021-08-21 14:26:38 +0300

    Add exploring tests for asserting throwables with AssertJ

---
+++ src/test/java/dev/boiarshinov/testing/assertions/assertj/ThrowableAssertingTest.java
@@ -0,0 +1,62 @@
+package dev.boiarshinov.testing.assertions.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class ThrowableAssertingTest {
+
+	@Test
+	void inFluentStyle() {
+		assertThatThrownBy(() -> { throw new MyException("exception message"); })
+			.isInstanceOf(MyException.class)
+			.hasMessage("exception message")
+			.hasNoCause();
+	}
+
+	// AAA - arrange, act, assert
+	@Test
+	void inStyleOfAAA() {
+		var exception = catchThrowable(() -> { throw new MyException("exception message"); });
+
+		assertThat(exception)
+			.isInstanceOf(MyException.class)
+			.hasMessage("exception message")
+			.hasNoCause();
+	}
+
+	@Test
+	void nothingThrown() {
+		var exception = catchThrowable(() -> doNothing());
+		//var 'exception' is null
+		assertThat(exception).doesNotThrowAnyException();
+	}
+
+    @Test
+	void throwWithCause() {
+		var rootException = new IllegalArgumentException("root exception message");
+
+		var exception = catchThrowable(() -> { throw new MyException("exception message", rootException); });
+
+		assertThat(exception)
+			.isInstanceOf(MyException.class)
+			.hasMessage("exception message")
+			.hasCauseInstanceOf(IllegalArgumentException.class)
+			.hasRootCauseMessage("root exception message");
+	}
+
+    private static class MyException extends RuntimeException {
+
+		public MyException(String message) {
+			super(message);
+		}
+		public MyException(String message, Throwable cause) {
+			super(message, cause);
+		}
+
+	}
+
+	private void doNothing() {}
+}
