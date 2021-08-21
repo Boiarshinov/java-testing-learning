bd0263f8575f457c411312fa10480d49538d5fab
boiarshinov <artem.borya@gmail.com>
2021-08-21 21:16:53 +0300

    Add withFallMessage tests for error messages overloading with AssertJ

--- src/test/java/dev/boiarshinov/testing/assertions/assertj/AssertionMessagesTest.java
+++ src/test/java/dev/boiarshinov/testing/assertions/assertj/AssertionMessagesTest.java
@@ -17,26 +17,60 @@ public class AssertionMessagesTest {
     }
 
     @Test
-    void failAtFirstAssertion() {
+    void asNotOverload() {
         assertThatThrownBy(
             () -> assertThat(ChronoUnit.HOURS)
                 .as("first message").isGreaterThan(ChronoUnit.WEEKS)
                 //second `as()` not overloads first one cause first assertion failed
                 .as("second message").isLessThan(ChronoUnit.DAYS))
-
             .hasMessageContaining("first message")
             .hasMessageNotContaining("second message");
     }
 
     @Test
-    void failAtSecondAssertion() {
+    void asOverload() {
         assertThatThrownBy(
             () -> assertThat(ChronoUnit.HOURS)
                 .as("first message").isGreaterThan(ChronoUnit.MILLIS)
                 //second `as()` overloads first one if first assertion was successful
                 .as("second message").isLessThan(ChronoUnit.NANOS))
-
             .hasMessageContaining("second message")
             .hasMessageNotContaining("first message");
+    }
+
+    @Test
+    void asFallThrow() {
+        assertThatThrownBy(
+            () -> assertThat(ChronoUnit.HOURS)
+                .as("first message").isGreaterThan(ChronoUnit.MILLIS)
+                //first `as()` not overloaded by anything
+                .isLessThan(ChronoUnit.NANOS))
+            .hasMessageContaining("first message");
+    }
+
+    @Test
+    void withFailMessage() {
+        assertThatThrownBy(
+            () -> assertThat(ChronoUnit.HOURS)
+                .withFailMessage("first message").isGreaterThan(ChronoUnit.WEEKS))
+            .hasMessage("first message");
+    }
+
+    @Test
+    void withFailMessageOverloading() {
+        assertThatThrownBy(
+            () -> assertThat(ChronoUnit.HOURS)
+                .withFailMessage("first message").isGreaterThan(ChronoUnit.MILLIS)
+                .withFailMessage("second message").isLessThan(ChronoUnit.NANOS))
+            .hasMessage("second message");
+    }
+
+    @Test
+    void withFailMessageFallThrow() {
+        assertThatThrownBy(
+            () -> assertThat(ChronoUnit.HOURS)
+                .withFailMessage("first message").isGreaterThan(ChronoUnit.MILLIS)
+                .isLessThan(ChronoUnit.NANOS))
+            .hasMessage("first message");
     }
 }
